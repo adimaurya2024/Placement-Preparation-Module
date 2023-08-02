8341f1a4632bb8b0596718982d2cdbcaec582b95
Aditya Maurya <115164737+adimaurya2024@users.noreply.github.com>
2023-08-02 23:35:34 +0530

    Update reverseLinkedList.cpp

--- Day 5/reverseLinkedList.cpp
+++ Day 5/reverseLinkedList.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        //Base case (if node is empty or only single node)
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        // Rest of the LL (recursively solved)
+        ListNode* rest= reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return rest;
+        
+    }
+};
