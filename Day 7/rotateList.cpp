655da51fd5bfd8154915251822b6ee0a5bef33f0
Aditya Maurya <115164737+adimaurya2024@users.noreply.github.com>
2023-08-03 00:12:55 +0530

    Create rotateList.cpp

---
+++ Day 7/rotateList.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode* temp = head;
+        int length = 0;
+        while(temp != NULL) {
+            temp = temp->next;
+            length++;
+        }
+        k = k % length;
+        if(k == 0)
+            return head;
+        int count = 0;
+        ListNode* end = head;
+        ListNode* ans = head->next;
+        ListNode* last = head;
+        while(count < k) {
+            end = end->next;
+            count++;
+        }
+        while(end->next != NULL) {
+            end = end->next;
+            ans = ans->next;
+            last = last->next;
+        }
+        end->next = head;
+        last->next = NULL;
+        return ans;
+        
+    }
+};
