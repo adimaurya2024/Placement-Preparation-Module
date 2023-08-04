9d0c76d3df97bf702f416996121b0cda1b89fda8
Aditya Maurya <115164737+adimaurya2024@users.noreply.github.com>
2023-08-04 23:27:12 +0530

    Create Search element in a sorted and rotated array.cpp

---
+++ Day 11/Search element in a sorted and rotated array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+            int n = nums.size();
+            int low = 0;
+            int high = n - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (nums[mid] == target)
+                    return mid;
+               	//Index found
+                else if (nums[low] <= nums[mid])
+               	//Left half
+                {
+                    if (nums[low] <= target && target <= nums[mid])
+                    {
+                    //right half eliminated
+                        high = mid - 1;
+                    }
+                    else
+                   	//left half eliminated
+                    {
+                        low = mid + 1;
+                    }
+                }
+                else
+                {
+                   	//right half
+                    if (nums[mid] <= target && target <= nums[high])
+                   	//left half eliminated
+                    {
+                        low = mid + 1;
+                    }
+                   	//right half eliminated
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+            }
+            //Index not found
+            return -1; 
+    }
+};
