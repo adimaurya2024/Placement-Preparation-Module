6cb150956ecc41eb4c6472447963f75d89fdb6e2
Aditya Maurya <115164737+adimaurya2024@users.noreply.github.com>
2023-08-18 22:26:00 +0530

    Create 	Zig Zag Traversal of Binary Tree.cpp

---
+++ Day 18/Zig Zag Traversal of Binary Tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+      if(root==NULL)
+      return {};
+      vector<vector <int >>ans;
+      queue<TreeNode*>q;
+      q.push(root);
+      bool flag= true;
+      while(!q.empty())
+      {
+          int size = q.size();
+          vector<int>res(size);
+          for(int i=0;i<size;i++)
+          {
+              TreeNode* temp=q.front();
+              q.pop();
+              int index=flag? i: size-1-i;
+              res[index]=temp->val;
+              if(temp->left)
+              {
+                  q.push(temp->left);
+              }
+              if(temp->right)
+              {
+                  q.push(temp->right);
+              }
+          }
+          ans.push_back(res);
+          flag=!flag;
+      }  
+      return ans;
+    }
+};
